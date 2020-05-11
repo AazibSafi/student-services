3022ca8994337448dfa969c2cef96b45c515c980
Aazib Safi <aazibsafi@gmail.com>
2020-05-11 00:29:02 -0400

    Created New SpringBoot Project

---
+++ src/test/java/com/education/studentServices/StudentServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.studentServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
